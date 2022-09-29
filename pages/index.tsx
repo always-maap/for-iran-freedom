29460ed7be956e2f9cc44d2630c539f0bcabf7f4
tur1ng <mohammadali.ap.2000@gmail.com>
2022-09-29 23:37:41 +0330

    height 100%

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-const quotes = ["برای آزادانه رقصیدن تو کوچه پس کوچه های کشورم"];
+const quotes = [
+  "برای آزادانه رقصیدن تو کوچه پس کوچه های کشورم",
+  "برای تمام روزهایی با ترس معشوقمان را در خیابان بوسیدیم",
+  `برای خواهرم
+خواهرت
+خواهرمون`,
+  "برای تغییر مغز های پوسیده",
+  "برای اون پدری که شرمنده خانوادش شده و کمرش زیر بار مشکلات خم شد...🖤",
+  "برای یه زندگی معمولی",
+];
 
 const Home: NextPage = () => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>
-        {quotes[Math.floor(Math.random() * quotes.length)]}
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>برای...</title>
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.title}>{randomQuote}</h1>
+      </div>
+    </>
   );
 };
 
